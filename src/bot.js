b789250ae11d209420e17f481268b4ab312a775f
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2022-03-02 20:40:32 -0300

    Adiciona as primeiras mensagens

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,48 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message } = require('discord.js');
 const { token } = require('../config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-/*
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+// EVENTOS
+client.on('ready', () => {
+    console.log(`${client.user.username} está online :)`);
 });
-*/
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+
+    // Nossa missão
+    if (message.content === 'nossa missão' ||
+        message.content === 'nossa missao' ||
+        message.content === 'nossamissao') {
+
+        message.channel.send('Promover o **empreendedorismo júnior** e **democratizar a transformação digital** através de soluções tecnológicas que contribuem para o desenvolvimento da região que atuamos.');
+    }
+
+    // Nossa Visão <3
+    if (message.content === 'nossa visão' ||
+        message.content === 'nossa visao' ||
+        message.content === 'nossavisao') {
+
+        message.channel.send('Ser uma empresa **referência em serviços de Tecnologia da Informação** \ne reconhecida pelo MEJ pela sua atuação, resultados e projetos de alto \nimpacto nas regiões do Médio Piracicaba e Vale do Aço.')
+    }
+
+    // Nossos valores
+    if (message.content === 'nossos valores' ||
+        message.content === 'nossosvalores') {
+
+            message.channel.send('🗸 Orgulho de ser visionário\n🗸 Empatia\n🗸 Transparência\n🗸 Resiliência\n🗸 Agilidade\n🗸 Responsabilidade Socioambiental\n🗸 Proatividade\n🗸 Colaboração\n')
+    }
+
+    // Nossa paleta de cores
+    if(message.content === 'nossa paleta') {
+        message.channel.send('#88B04B, #292929, #1F1F28');
+    }
+
+    // Teste
+    if (message.content === 'teste') {
+        message.channel.send('Quem testa é o usuário.')
+    }
+});
 
 client.login(token);
